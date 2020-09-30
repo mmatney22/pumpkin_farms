924768c58a8f10efdb088aa7679cf3e33692dfc8
Morgan Matney <morgan.matney89@gmail.com>
2020-09-30 21:54:25 +0000

    Modify puts messages

--- MA_pumpkin_farms/lib/MA_pumpkin_farms/cli.rb
+++ MA_pumpkin_farms/lib/MA_pumpkin_farms/cli.rb
@@ -13,22 +13,23 @@ class MAPumpkinFarms::CLI
   end
   
   def list_states
-    puts "Choose a state to see a list of farms."
+    puts "Choose a state to see a list of pumpkin farms."
     @states.each.with_index(1) {|state, index| puts "#{index}. #{state}"}    #iterates over array, and lists states
   end
   
   def get_user_state
-    chosen_state = gets.strip
-    show_farms_for(chosen_state) if valid_input?(chosen_state, @states)        #if valid input, show list of farms
+    chosen_state = gets.strip.to_i
+    list_farms_for(chosen_state) if valid_input?(chosen_state, @states)        #if valid input, show list of farms
   end
   
   def valid_input?(input, data)
     input.to_i <= data.length && input.to_i > 0        #change input to integar and check validity
   end
   
-  def show_farms_for(chosen_state)
+  def list_farms_for(chosen_state)
     state = @states[chosen_state - 1]              #set variable to array item of @state, minus 1 for correct index
-    puts "Here are pumpkin farms for #{state}"
+    puts "Here are pumpkin farms located in #{state}"
+    binding.pry
   end
   
   #get_user_farm
