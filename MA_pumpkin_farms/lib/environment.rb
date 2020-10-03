5cf2422e0fce8a4e21464afde728f60728657a51
Morgan Matney <morgan.matney89@gmail.com>
2020-10-03 15:30:59 +0000

    change class and method names for easier scrape

--- MA_pumpkin_farms/lib/environment.rb
+++ MA_pumpkin_farms/lib/environment.rb
@@ -1,6 +1,6 @@
 require_relative "./MA_pumpkin_farms/version"
 require_relative "./MA_pumpkin_farms/cli"
-require_relative "./MA_pumpkin_farms/state"
+require_relative "./MA_pumpkin_farms/farm"
 require_relative "./MA_pumpkin_farms/scraper"
 
 require 'pry'
